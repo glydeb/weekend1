f02c536d6300f8dafdb14d44d6f69123fb66916f
Glyde <glydeb@yahoo.com>
2016-04-29 15:48:39 -0500

    Initial commit

---
+++ forms.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    var array = [];
+    $('#employeeinfo').on('submit', function(event) {
+      event.preventDefault();
+
+      var values = {};
+      $.each($('#employeeinfo').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+      })
+
+      console.log(values);
+      // clear out inputs
+      $('#employeeinfo').find('input[type=text]').val('');
+
+      appendDom(values);
+    });
+
+    function appendDom(empInfo) {
+      $('#container').append('<div class="person"></div>');
+      var $el = $('#container').children().last();
+
+      $el.append('<p>' + empInfo.employeefirstname + '</p>');
+    }
+
+
+});
