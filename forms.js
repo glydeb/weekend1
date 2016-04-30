23b9eb1d2315efb8f9b9ba36c983a99e1706b02c
Glyde <glydeb@yahoo.com>
2016-04-30 17:21:04 -0500

    add total salaries and some input checking

--- forms.js
+++ forms.js
@@ -1,19 +1,61 @@
 $(document).ready(function () {
-  var array = [];
+  var totalSalaries = 0;
   $('#employeeinfo').on('submit', function (event) {
     event.preventDefault();
     var values = {};
+    var salary = 0;
+    var employeeIDs = [];
     $.each($('#employeeinfo').serializeArray(), function (i, field) {
       values[field.name] = field.value;
     });
 
     console.log(values);
 
-    // clear out inputs
-    //$('#employeeinfo').find('input[type=text]').val('');
+    // check salary for spaces and reject if found
+    // otherwise, process it
+    var spaces = checkSalary(values.employeeSalary);
+    var unique = checkID(values.employeeID);
+    if (!spaces && unique) {
+      salary = parseFloat(values.employeeSalary);
+      if (isNaN(salary)) {
+        alert('Invalid salary entered - please revise & resubmit');
+      } else {
+        totalSalaries += salary;
 
-    appendEmployee(values);
+        // clear out inputs
+        //$('#employeeinfo').find('input[type=text]').val('');
+        modifySalaryTotal(totalSalaries);
+        appendEmployee(values);
+      }
+    }
   });
+
+  function checkSalary(salaryString) {
+    for (var i = 0; i < salaryString.length; i++) {
+      if (salaryString.charAt(i) === ' ') {
+        alert('Invalid salary entered - please revise & resubmit');
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function checkID(empID) {
+    for (var i = 0; i < employeeIDs.length; i++) {
+      if (empID === employeeIDs[i]) {
+        alert('That ID is alredy in use - please revise & resubmit');
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function modifySalaryTotal(total) {
+    $('aside').empty();
+    $('aside').text('Total Annual Salaries: $' + total);
+  }
 
   function appendEmployee(empInfo) {
     $('#container').append('<div class="person"></div>');
