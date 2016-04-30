3fdc11277d668268731ae38b60e00b8014e9391b
Glyde <glydeb@yahoo.com>
2016-04-30 15:46:48 -0500

    update for added button, conversion to a list

--- forms.js
+++ forms.js
@@ -1,26 +1,34 @@
-$(document).ready(function() {
-    var array = [];
-    $('#employeeinfo').on('submit', function(event) {
-      event.preventDefault();
-
-      var values = {};
-      $.each($('#employeeinfo').serializeArray(), function(i, field) {
-        values[field.name] = field.value;
-      })
-
-      console.log(values);
-      // clear out inputs
-      $('#employeeinfo').find('input[type=text]').val('');
-
-      appendDom(values);
+$(document).ready(function () {
+  var array = [];
+  $('#employeeinfo').on('submit', function (event) {
+    event.preventDefault();
+    var values = {};
+    $.each($('#employeeinfo').serializeArray(), function (i, field) {
+      values[field.name] = field.value;
     });
 
-    function appendDom(empInfo) {
-      $('#container').append('<div class="person"></div>');
-      var $el = $('#container').children().last();
+    console.log(values);
 
-      $el.append('<p>' + empInfo.employeefirstname + '</p>');
-    }
+    // clear out inputs
+    //$('#employeeinfo').find('input[type=text]').val('');
 
+    appendEmployee(values);
+  });
+
+  function appendEmployee(empInfo) {
+    $('#container').append('<div class="person"></div>');
+
+    // Set target to the created div and append a list and button
+    var $el = $('#container').children().last();
+    $el.append('<ul></ul><button>Delete Employee</button>');
+
+    // Set target to the created list element and insert employee
+    $el = $('#container').children().last().find('ul');
+    $el.append('<li>' + empInfo.employeefirstname + '</li>');
+    $el.append('<li>' + empInfo.employeelastname + '</li>');
+    $el.append('<li>' + empInfo.employeeID + '</li>');
+    $el.append('<li>' + empInfo.employeeTitle + '</li>');
+    $el.append('<li>' + empInfo.employeeSalary + '</li>');
+  }
 
 });
